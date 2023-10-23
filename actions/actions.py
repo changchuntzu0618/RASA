4dd86b57e4df09991eff26010769094c1cbed4e1
changchuntzu0618 <chun-tzu.chang@epfl.ch>
2023-10-23 17:13:10 +0200

    integrate chatgpt

--- actions/actions.py
+++ actions/actions.py
@@ -11,18 +11,50 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+import os
+import openai
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+    
+
+class ActionGptJoke(Action):
+
+    def name(self) -> Text:
+        self.openai=openai
+        self.openai.api_key = os.getenv("OPENAI_API_KEY")
+        return "action_gpt_joke"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        last_message = tracker.latest_message['text']
+
+        completion = self.openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a joke provider. Provide a new joke everytime to make people happy. Remember, do not provide the joke which is provided before"},
+                # {"role": "user", "content":last_message},
+            ],
+            temperature=1.5,
+        )
+
+        answer = completion.choices[0].message["content"]
+
+        dispatcher.utter_message(text=answer)
+
+        return []
 
